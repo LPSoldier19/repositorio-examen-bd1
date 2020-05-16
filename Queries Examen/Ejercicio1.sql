6859985b0389e98043c6e4b17eb1bf2ab6b95dd3
LPSoldier19 <rafael.bautista1@hotmail.es>
2020-05-16 17:18:02 -0600

    Ya funciona todo el Query del Ejercicio 1

--- Queries Examen/Ejercicio1.sql
+++ Queries Examen/Ejercicio1.sql
@@ -1,3 +1,41 @@
+WITH 
+    Promedio AS (
+        SELECT 
+            s.SupplierID as SupplierID,
+            MONTH(o.OrderDate) as mes,
+            AVG(od.QUantity * p.Price) as promedio
+        FROM example.Customers as c
+        INNER JOIN example.[Order] as o 
+        ON c.CustomerID = o.CustumerID
+        INNEr JOIN example.OrderDetail as od 
+        ON o.OrderID = od.OrderID
+        INNER JOIN example.product as p 
+        ON od.ProductID = p.ProductoID
+        INNER JOIN example.Supplier as s 
+        ON p.SupplierID = s.SupplierID
+        GROUP BY 
+            s.SupplierID,
+            MONTH(o.OrderDate)),
+
+    VentaMensual AS (
+        SELECT 
+            c.CustomerID as CustomerID,
+            SUM(od.QUantity * p.Price) as SumaVentas
+        FROM example.Customers as c
+        INNER JOIN example.[Order] as o 
+        ON c.CustomerID = o.CustumerID
+        INNEr JOIN example.OrderDetail as od 
+        ON o.OrderID = od.OrderID
+        INNER JOIN example.product as p 
+        ON od.ProductID = p.ProductoID
+        INNER JOIN example.Supplier as s 
+        ON p.SupplierID = s.SupplierID
+        GROUP BY 
+            c.CustomerID
+    )
+
+
+
 SELECT 
     c.CustomerID,
     c.CustomerName,
@@ -5,7 +43,12 @@ SELECT
     s.SupplierName,
     MONTH(o.OrderDate) as mes,
     YEAR (o.OrderDate) as anio,
-    SUM(od.QUantity * p.Price) as total
+    SUM(od.QUantity * p.Price) as total,
+    CASE
+        WHEN SUM(od.QUantity * p.Price) < sp.promedio THEN 0
+        ELSE 1
+    END AS SuperoPromedio,
+    SUM(od.QUantity * p.Price)/v.SumaVentas as PorcentajeVentaMensual
 FROM example.Customers as c
 INNER JOIN example.[Order] as o 
 ON c.CustomerID = o.CustumerID
@@ -15,11 +58,18 @@ INNER JOIN example.product as p
 ON od.ProductID = p.ProductoID
 INNER JOIN example.Supplier as s 
 ON p.SupplierID = s.SupplierID
+INNER JOIN Promedio as sp
+ON sp.SupplierID = s.SupplierID and sp.mes = MONTH(o.OrderDate)
+INNER JOIN VentaMensual as v 
+ON v.CustomerID = c.CustomerID
 GROUP BY 
     c.CustomerID,
     c.CustomerName,
     s.SupplierID,
     s.SupplierName,
     MONTH(o.OrderDate),
-    YEAR(o.OrderDate)
+    YEAR(o.OrderDate),
+    sp.promedio,
+    v.SumaVentas
+
 
